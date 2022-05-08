b3220af2905185b27aba6dcff86228552a5c4d5e
maryh1001 <focusedfitness.mh@gmail.com>
2022-05-07 21:48:42 -0400

    adding places controller

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // require needed modules
+require('dontenv').config()
 const express = require('express')
 
 // initialize
 const app = express()
+
+// import router from places
+app.use('/places', require('./controllers/places'))
 
 // homepage route
 app.get('/', function (req, res) {
     res.send('hello')
 })
 
+app.get('*', (rew, res) => {
+    res.status(404).send ('<h1> 404 Page</h1>')
+})
+
 // listen
-app.listen(3000)
+app.listen(process.env.PORT)
 
